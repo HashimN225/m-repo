f7cde3beecca20e8a424273763b3400998d0df87
SravyaCrunchOps <sravya@crunchops.com>
2026-02-10 06:54:22 +0530

    git version update

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="employee_attrition_project",
     version="0.1.0",
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     install_requires=["pandas", "mlflow", "boto3"],
-    package_dir={"": "src"}
 )
